ad82ffdba7817295fadcb5aba68de8da05d5856c
Alejandro <alejandrofm1993@gmail.com>
2013-09-03 22:11:15 +0200

    Revert "HALO Size (2/2)"
    This reverts commit f834f319a7a7a2b70b50e8da57ea76d4bbf3fd79.

--- src/com/android/settings/cyanogenmod/HaloControl.java
+++ src/com/android/settings/cyanogenmod/HaloControl.java
@@ -27,12 +27,10 @@ public class HaloControl extends SettingsPreferenceFragment implements Preferenc
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
-    private static final String KEY_HALO_SIZE = "halo_size";
 
     private ListPreference mHaloState;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
-    private ListPreference mHaloSize;
 
     private Context mContext;
     private INotificationManager mNotificationManager;
@@ -59,17 +57,6 @@ public class HaloControl extends SettingsPreferenceFragment implements Preferenc
         mHaloReversed = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_REVERSED);
         mHaloReversed.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_REVERSED, 1) == 1);
-
-        mHaloSize = (ListPreference) prefSet.findPreference(KEY_HALO_SIZE);
-        try {
-            float haloSize = Settings.System.getFloat(mContext.getContentResolver(),
-                    Settings.System.HALO_SIZE, 1.0f);
-            mHaloSize.setValue(String.valueOf(haloSize));  
-        } catch(Exception ex) {
-            // So what
-        }
-        mHaloSize.setOnPreferenceChangeListener(this);
-
     }
 
     private boolean isHaloPolicyBlack() {
@@ -104,11 +91,6 @@ public class HaloControl extends SettingsPreferenceFragment implements Preferenc
             } catch (android.os.RemoteException ex) {
                 // System dead
             }          
-            return true;
-        } else if (preference == mHaloSize) {
-            float haloSize = Float.valueOf((String) newValue);
-            Settings.System.putFloat(getActivity().getContentResolver(),
-                    Settings.System.HALO_SIZE, haloSize);
             return true;
         }
         return false;
